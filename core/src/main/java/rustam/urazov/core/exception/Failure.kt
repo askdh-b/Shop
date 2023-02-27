85b74c48ac933610961ad30e8196721725ddd963
askdh-b <rustpy2703@gmail.com>
2023-02-27 12:19:33 +0600

    add exception

---
+++ core/src/main/java/rustam/urazov/core/exception/Failure.kt
@@ -0,0 +1,8 @@
+package rustam.urazov.core.exception
+
+sealed class Failure {
+    object NoError : Failure()
+    object ConnectionError : Failure()
+    data class ServerError(val message: String) : Failure()
+    data class MemoryError(val message: String) : Failure()
+}
