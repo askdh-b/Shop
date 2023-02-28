edaceaa66353a7628a4a103470f3e3ee5189bd01
askdh-b <rustpy2703@gmail.com>
2023-02-28 17:53:17 +0600

    update sign in fragment

--- app/src/main/java/rustam/urazov/shop/screens/signin/SignInFragment.kt
+++ app/src/main/java/rustam/urazov/shop/screens/signin/SignInFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import rustam.urazov.core.extension.empty
+import rustam.urazov.core.extension.viewBinding
 import rustam.urazov.core.platform.BaseFragment
 import rustam.urazov.feature_sign_in.SignInViewModel
 import rustam.urazov.feature_sign_in.models.UserView
@@ -16,40 +16,38 @@ import rustam.urazov.shop.databinding.FragmentSignInBinding
 @AndroidEntryPoint
 class SignInFragment : BaseFragment(R.layout.fragment_sign_in) {
 
-    private var viewBinding: FragmentSignInBinding? = null
+    private val viewBinding by viewBinding { FragmentSignInBinding.bind(it) }
     private val viewModel by viewModels<SignInViewModel>()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewBinding = FragmentSignInBinding.bind(view)
+        with(viewBinding) {
+            etEmail.addTextChangedListener {
+                viewModel.handleEmail(etEmail.text.toString())
+            }
 
-        viewBinding?.etEmail?.addTextChangedListener {
-            viewModel.handleEmail(viewBinding?.etEmail?.text.toString())
-        }
+            bSignIn.setOnClickListener {
+                viewModel.signIn(UserView(
+                    firstName = etFirstName.text.toString(),
+                    lastName = etLastName.text.toString(),
+                    email = etEmail.text.toString(),
+                    password = String.empty()
+                ))
+            }
 
-        viewBinding?.bSignIn?.setOnClickListener {
-            viewModel.signIn(UserView(
-                firstName = viewBinding?.etFirstName?.text.toString(),
-                lastName = viewBinding?.etLastName?.text.toString(),
-                email = viewBinding?.etEmail?.text.toString(),
-                password = String.empty()
-            ))
-        }
-
-        lifecycleScope.launchWhenStarted {
-            viewModel.emailValidationState.collect { error ->
+            viewModel.emailValidationState.collectWhileStarted { error ->
                 when (error.isEmpty()) {
                     true -> {
-                        viewBinding?.etEmail?.setBackgroundResource(R.drawable.view_sign_text_field)
+                        etEmail.setBackgroundResource(R.drawable.view_sign_text_field)
                     }
                     false -> {
-                        viewBinding?.etEmail?.setBackgroundResource(R.drawable.view_sign_text_field_error)
-                        viewBinding?.tvEmail?.text = error
+                        etEmail.setBackgroundResource(R.drawable.view_sign_text_field_error)
+                        tvEmail.text = error
                     }
                 }
             }
         }
+
     }
 
 }
